71e966aba6b5772e05b7d7649af098f65c1f7d4a
priyasdk <priya.sdk19@gmail.com>
2017-08-25 21:49:09 +0530

    Program to understand if-else better

---
+++ testing1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main()
+{
+  int i;
+  for(i=1;i<30;i++)
+  {
+    if(i%15==0)
+    printf("FizzBuzz\n");
+     if(i%3==0)
+    printf("Buzz\n");
+     if(i%5==0)
+    printf("Fizz\n");
+    else
+    printf("%d\n",i);
+  }
+}
